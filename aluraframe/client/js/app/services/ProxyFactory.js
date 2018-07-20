178c20e12a4c8152d4a9f5f15fe44c3d81a677f1
Felipe Lima <felipelima94@live.com>
2018-07-20 12:11:16 -0300

    implementaçã api Rest

--- aluraframe/client/js/app/services/ProxyFactory.js
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -1,19 +1,36 @@
-class ProxyFactory {
-    static create (objeto, props, acao) {
+class ProxyFactory{
+
+    static create(objeto, props, acao) {
+
         return new Proxy(objeto, {
 
             get(target, prop, receiver) {
 
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
+
                     return function() {
 
                         console.log(`a propriedade "${prop}" foi interceptada`);
                         Reflect.apply(target[prop], target, arguments);
                         return acao(target);
-                    }
+                    };
                 }
                 return Reflect.get(target, prop, receiver);       
+           },
+
+            set(target, prop, value, receiver) {
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }
+                return Reflect.set(target, prop, value, receiver);
             }
-        })
+      });
+    }
+
+    static _ehFuncao(func) {
+
+        return typeof(func) == typeof(Function);
+    
     }
 }
