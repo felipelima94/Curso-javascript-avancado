e995d1ead49d16e718c6bad4ff43034635d24143
Felipe Lima <felipelima94@live.com>
2018-07-20 16:08:00 -0300

    XMLHttpRequest

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,27 +1,26 @@
 class ListaNegociacoes {
-    
+
     constructor() {
         
         this._negociacoes = [];
-        // this._armadilha = armadilha;
-        // this._contexto = contexto;
     }
-    
+
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-        // this._armadilha(this);
-        // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
-    
+
     get negociacoes() {
         
         return [].concat(this._negociacoes);
     }
 
     esvazia() {
+        
         this._negociacoes = [];
-        // this._armadilha(this);
-        // Reflect.apply(this._armadilha, this._contexto, [this]);
+    }
+    
+    get volumeTotal() {
+       return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
     }
 }
