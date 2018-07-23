08b6c09200e97c80c002e61a5f1b94f230fa78f6
Felipe Lima <felipelima94@live.com>
2018-07-23 17:58:31 -0300

    Lapidando um pouco nossa aplicação

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -18,39 +18,33 @@ class NegociacaoController {
             new Mensagem(), new MensagemView($('#mensagemView')),
             'texto');
 
-        ConnectionFactory
-            .getConnection()
-            .then(connection => new NegociacaoDao(connection))
-            .then(dao => dao.listaTodos())
+        this._init();
+    }
+
+    _init() {
+        new NegociacaoService()
+            .lista()
             .then(negociacoes =>
                 negociacoes.forEach(negociacao =>
                     this._listaNegociacoes.adiciona(negociacao)))
-            .catch(erro => {
-                console.log(erro);
-                this._mensagem.texto = erro;
-            });
+            .catch(erro => this._mensagem.texto = erro);
+
+        setInterval(() => {
+            this.importaNegociacoes()
+        }, 3000);
     }
     
     adiciona(event) {
         event.preventDefault();
-        
-        ConnectionFactory
-            .getConnection()
-            .then(conexao => {
 
-                let negociacao = this._criaNegociacao();
+        let negociacao = this._criaNegociacao();
 
-                new NegociacaoDao(conexao)
-                    .adiciona(negociacao)
-                    .then(() => {
-                        this._listaNegociacoes.adiciona(negociacao);
-                        this._mensagem.texto = 'Negociação adicionada com sucesso';
-                        this._limpaFormulario();   
-                    })
-                    .catch(erro => {
-                        console.log(erro);
-                        this._mensagem.texto = erro;
-                    });
+        new NegociacaoService()
+            .cadastra(negociacao)
+            .then(mensagem => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = mensagem;
+                this._limpaFormulario();
             })
             .catch(erro => this._mensagem.texto = erro);
     }
@@ -60,6 +54,11 @@ class NegociacaoController {
         let service = new NegociacaoService();
         service
             .obterNegociacoes()
+            .then(negociacoes =>
+                negociacoes.filter(negociacao =>
+                    !this._listaNegociacoes.negociacoes.some(negociacaoExistente =>
+                        JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
+            )
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.adiciona(negociacao);
                 this._mensagem.texto = 'Negociações do período importadas'   
--- aluraframe/client/js/app/views/MensagemView.js
+++ aluraframe/client/js/app/views/MensagemView.js
@@ -7,6 +7,8 @@ class MensagemView extends View {
     
    template(model) {
        
-       return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
+       return model.texto ? `<p class="alert alert-info">${model.texto}` : '<p></p>';
    }
+
+   
 }
